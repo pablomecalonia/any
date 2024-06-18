9fb66b6cda3fd0ca7d61bd14cc7078e0e2e03300
pablomecalonia <143449394+pablomecalonia@users.noreply.github.com>
2024-06-18 17:49:13 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Variaveis
-DEB_URL="https://raw.githubusercontent.com/pablomecalonia/any/anydesk_6.3.0-1_armhf.deb"
-CONFIG_URL="https://raw.githubusercontent.com/pablomecalonia/any/anydesk"
+DEB_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk_6.3.0-1_armhf.deb"
+CONFIG_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk"
 # Baixar o arquivo .deb
 wget $DEB_URL -O /tmp/anydesk_6.3.0-1_armhf.deb
 # Instalar o AnyDesk
