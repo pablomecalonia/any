460fcb6976ddc9fb0ddefb75a8e2b0f7625f1a5d
pablomecalonia <143449394+pablomecalonia@users.noreply.github.com>
2024-06-18 17:45:47 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -5,13 +5,13 @@ CONFIG_URL="https://raw.githubusercontent.com/SEU_USUARIO/anydesk-raspberry-inst
 # Baixar o arquivo .deb
 wget $DEB_URL -O /tmp/anydesk_6.3.0-1_armhf.deb
 # Instalar o AnyDesk
-sudo dpkg -i /tmp/anydesk_6.3.0-1_armhf.deb
+#sudo dpkg -i /tmp/anydesk_6.3.0-1_armhf.deb
 # Parar o serviço AnyDesk
-sudo systemctl stop anydesk
-sudo systemctl stop anydesk.service
+#sudo systemctl stop anydesk
+#sudo systemctl stop anydesk.service
 # Baixar o arquivo de configuração
-wget $CONFIG_URL -O /tmp/anydesk
+#wget $CONFIG_URL -O /tmp/anydesk
 # Mover o arquivo de configuração para o diretório correto
-sudo mv /tmp/anydesk /etc/anydesk/
+#sudo mv /tmp/anydesk /etc/anydesk/
 # Reiniciar o serviço AnyDesk
-sudo systemctl restart anydesk
+#sudo systemctl restart anydesk
