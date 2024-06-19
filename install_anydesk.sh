52eecabc4a2be2e2b1d3bcee6e9e7317d16bca9f
pablomecalonia <143449394+pablomecalonia@users.noreply.github.com>
2024-06-19 09:27:03 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -2,25 +2,30 @@
 # Variaveis
 echo ">>>>>> estabelecendo conexão"
 DEB_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk_6.3.0-1_armhf.deb"
-CONFIG_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk"
+TAR_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/AD_Meca.tar.gz"
+TMP_DIR="/tmp/anydesk_install"
+ANYDESK_DIR="$TMP_DIR/anydesk-6.3.0"
+# Criar diretório temporário
+mkdir -p $TMP_DIR
 # Baixar o arquivo .deb
 echo ">>>>>> baixando .deb"
 wget $DEB_URL -O /tmp/anydesk_6.3.0-1_armhf.deb
-# Baixar o arquivo de configuração
 echo ">>>>>> baixando anydesk"
-wget $CONFIG_URL -O /tmp/anydesk
+wget $TAR_URL -O $TMP_DIR/AD_Meca.tar.gz
+# Baixar o arquivo de configuração
+#echo ">>>>>> baixando anydesk"
+#wget $CONFIG_URL -O /tmp/anydesk
 # Instalar o AnyDesk
 echo ">>>>>> instalando"
-sudo dpkg -i /tmp/anydesk_6.3.0-1_armhf.deb
+# Instalar o AnyDesk
+sudo dpkg -i $TMP_DIR/anydesk_6.3.0-1_armhf.deb
 # && sudo systemctl stop anydesk && sudo systemctl stop anydesk.service && 
 echo ">>>>>> movendo arquivo para anydesk"
-sudo cp /tmp/anydesk /etc/anydesk/
-#&& sudo systemctl restart anydesk
-sudo cp /tmp/anydesk /usr/local/bin/
-# Parar o serviço AnyDesk
-#sudo systemctl stop anydesk
-#sudo systemctl stop anydesk.service
-# Mover o arquivo de configuração para o diretório correto
-#sudo mv /tmp/anydesk /etc/anydesk/
+# Copiar o arquivo de configuração para o diretório correto
+sudo cp $ANYDESK_DIR/anydesk /etc/anydesk/
+sudo cp $ANYDESK_DIR/anydesk /usr/local/bin/
 # Reiniciar o serviço AnyDesk
-#sudo systemctl restart anydesk
+sudo systemctl restart anydesk
+anydesk
+# Limpar arquivos temporários
+rm -rf $TMP_DIR
