80b8e7b37aaf5b1e52e7d6ec992e444e1c5f19bc
pablomecalonia <143449394+pablomecalonia@users.noreply.github.com>
2024-06-19 08:38:28 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Variaveis
+echo ">>>>>> estabelecendo conexão"
 DEB_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk_6.3.0-1_armhf.deb"
 CONFIG_URL="https://raw.githubusercontent.com/pablomecalonia/any/main/anydesk"
 # Baixar o arquivo .deb
+echo ">>>>>> baixando .deb"
 wget $DEB_URL -O /tmp/anydesk_6.3.0-1_armhf.deb
 # Baixar o arquivo de configuração
+echo ">>>>>> baixando anydesk"
 wget $CONFIG_URL -O /tmp/anydesk
 # Instalar o AnyDesk
-sudo dpkg -i /tmp/anydesk_6.3.0-1_armhf.deb && sudo systemctl stop anydesk && sudo systemctl stop anydesk.service && sudo mv /tmp/anydesk /etc/anydesk/ && sudo systemctl restart anydesk
+echo ">>>>>> instalando"
+sudo dpkg -i /tmp/anydesk_6.3.0-1_armhf.deb # && sudo systemctl stop anydesk && sudo systemctl stop anydesk.service && 
+echo ">>>>>> movendo arquivo para anydesk"
+sudo mv /tmp/anydesk /etc/anydesk/ && sudo systemctl restart anydesk
 # Parar o serviço AnyDesk
 #sudo systemctl stop anydesk
 #sudo systemctl stop anydesk.service
